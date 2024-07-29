6fe6462027c33540de4a696505c26d1b45c64489
Donvanesi <donvanesi1337@gmail.com>
2024-07-29 20:01:05 +0300

    Add files via upload

--- ConsoleApplication17.cpp
+++ ConsoleApplication17.cpp
@@ -1,113 +1,78 @@
 ﻿#include <iostream>
-#include <string>
-#include <map>
 using namespace std;
 
-struct employee
+
+
+struct calculator
 {
-	char etype;
-	struct salary;
-	struct date;
-};
-struct date
-{
-	int day;
-	int month;
-	int year;
-};
-struct salary
-{
-	int number;
-	int salary;
+	int first_number;
+	int second_number;
+	int result;
 };
 
-enum etype { laborer = 1 , secretary, manager, accountant, executive, researcher };
+void fadd(calculator&);
+void fsub(calculator&);
+void fmul(calculator&);
+void fdiv(calculator&);
 
 int main()
 {
-	int x = 1;
-	string delimiter = "/";
-	string ch;
-	char symbol;
-	int pos = 0;
-	int count = 0;
-	string token;
-	setlocale(LC_ALL, "Russian");
-	salary salary1;
-	
-	
-	cout << "Введите номер сотрудника : ";
-	cin >> salary1.number;
-	cout << "Введите зарплату сотрудника";
-	cin >> salary1.salary;
-	cout << "Введите дату приема сотруднкиа на работу в формате XX/XX/XX : ";
-	date date1;
+	setlocale(LC_ALL, "Russiаn");
+	char ch;
+	calculator calculator1;
+	cout << "Введите первое число: ";
+	cin >> calculator1.first_number;
+	cout << "Введите второе число: ";
+	cin >> calculator1.second_number;
+	cout << "Выберите какое действие выполнить: + - / *";
 	cin >> ch;
-	while ((pos = ch.find(delimiter)))
-
+	switch (ch)
 	{
-		if (pos != string::npos)
-		{
-			token = ch.substr(0, pos);
-			if (count == 0)
-				date1.day = atoi(token.c_str());
-			else
-				date1.month = atoi(token.c_str());
-				
-			count++;
-			ch.erase(0, pos + delimiter.length());
-		}
-
-		else
-		{
-			date1.year = atoi(ch.c_str());
-			break;
-		}
-	}
-
-	
-	etype person1;
-	map <int, string> job_title;
-	cout << "Введите первую букву должности сотрудника : ";
-	cin >> symbol;
-	switch (symbol)
+	case '+':
 	{
-	case 'L':
-		person1 = laborer;
-		job_title.insert(pair<int, string>(person1, "laborer"));
-		break;
-	case 'S':
-		person1 = secretary;
-		job_title.insert(pair<int, string>(person1, "secretary"));
-		break;
-	case 'M':
-		person1 = manager;
-		job_title.insert(pair<int, string>(person1, "manager"));
-		break;
-	case 'A':
-		person1 = accountant;
-		job_title.insert(pair<int, string>(person1, "accountant"));
-		break;
-	case 'E':
-		person1 = executive;
-		job_title.insert(pair<int, string>(person1, "executive"));
-		break;
-	case 'R':
-		person1 = researcher;
-		job_title.insert(pair<int, string>(person1, "researcher"));
+		fadd(calculator1);
+		cout << calculator1.result << endl;
 		break;
 	}
-	for (auto& line : job_title)
+	case '-':
 	{
-		cout << line.second << endl;
+		fsub(calculator1);
+		cout << calculator1.result << endl;
+		break;
 	}
-	
+	case '/':
+	{
+		fdiv(calculator1);
+		cout << calculator1.result << endl;
+		break;
+	}
+	case '*':
+	{
+		fmul(calculator1);
+		cout << calculator1.result << endl;
+		break;
+	}
+	default:
+	{
+		cout << "Вы ввели неправильный символ" << endl;
+		break;
+	}
+	return 0;
+	}
 }
-
-
-
-
-
-
-
-
+void fadd(calculator& plus)
+{
+	plus.result = plus.first_number + plus.second_number;
+}
+void fsub(calculator& minus)
+{
+	minus.result = minus.first_number - minus.second_number;
+}
+void fmul(calculator& multiplication)
+{
+	multiplication.result = multiplication.first_number * multiplication.second_number;
+}
+void fdiv(calculator& division)
+{
+	division.result = division.first_number / division.second_number;
+}
